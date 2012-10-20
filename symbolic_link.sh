cc726eec754926776fa5ef260a651bd8b9bf624c
KAWAMORITA Mitsuaki   email = kawa@hongo.wide.ad.jp [color] <mk@kawabook.local>
2012-10-20 14:52:23 +0900

    加筆修正

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-rm -f ~/.bash_profile
-rm -f ~/.bashrc
-rm -f ~/.vimrc
-rm -f ~/.git-completion.bash
-rm -f ~/.git-prompt.sh
-rm -f ~/.gitconfig
+echo "removing dot-files..."
+rm ~/.bash_profile
+rm ~/.bashrc
+rm ~/.vimrc
+rm ~/.git-completion.bash
+rm ~/.git-prompt.sh
+rm ~/.gitconfig
 
+echo "linking dot-files..."
 ln -s ~/my_repos/dotfiles-mac/.bash_profile ~/.bash_profile
 ln -s ~/my_repos/dotfiles-mac/.bashrc ~/.bashrc
 ln -s ~/my_repos/dotfiles-mac/.vimrc ~/.vimrc
@@ -14,5 +16,10 @@ ln -s ~/my_repos/dotfiles-mac/.git-completion.bash ~/.git-completion.bash
 ln -s ~/my_repos/dotfiles-mac/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/my_repos/dotfiles-mac/.gitconfig ~/.gitconfig
 
+echo "activating dot-files..."
 source ~/.bash_profile
 source ~/.vimrc
+source ~/.git-completion.bash
+source ~/.git-prompt.sh
+
+# source ~/.gitconfig ←これはいらない。gitが勝手に読むので関係ない。
