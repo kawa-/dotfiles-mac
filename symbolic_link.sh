d5e9df62e7faa25f60db67eedd7080bbd242022c
KAWAMORITA Mitsuaki <kawa@hongo.wide.ad.jp>
2012-10-20 14:41:41 +0900

    いろいろと追加、修正。主にgitconfig。

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -5,13 +5,14 @@ rm -f ~/.bashrc
 rm -f ~/.vimrc
 rm -f ~/.git-completion.bash
 rm -f ~/.git-prompt.sh
+rm -f ~/.gitconfig
 
 ln -s ~/my_repos/dotfiles-mac/.bash_profile ~/.bash_profile
 ln -s ~/my_repos/dotfiles-mac/.bashrc ~/.bashrc
 ln -s ~/my_repos/dotfiles-mac/.vimrc ~/.vimrc
 ln -s ~/my_repos/dotfiles-mac/.git-completion.bash ~/.git-completion.bash
 ln -s ~/my_repos/dotfiles-mac/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/my_repos/dotfiles-mac/.gitconfig ~/.gitconfig
 
 source ~/.bash_profile
-source ~/.bashrc
 source ~/.vimrc
